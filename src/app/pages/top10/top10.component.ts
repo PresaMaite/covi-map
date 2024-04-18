306553da7470fb04e025db819a3b2cbbf16b5641
PresaMaite <m.presa@hotmail.com>
2024-04-18 17:23:06 +0200

    feat(top10): fetch and sort

--- src/app/pages/top10/top10.component.ts
+++ src/app/pages/top10/top10.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FetchDataService } from '../../services/fetch-data/fetch-data.service';
 
 @Component({
   selector: 'app-top10',
@@ -7,6 +8,77 @@ import { Component } from '@angular/core';
   templateUrl: './top10.component.html',
   styleUrl: './top10.component.css'
 })
-export class Top10Component {
+export class Top10Component implements OnInit {
+  topCases: any [] = [];
+  topDeaths: any[] = [];
+  topActives: any[] = [];
+  topRecovered: any[] = [];
+  topTodayDeaths: any[] = [];
+  topTodayCases: any[] = [];
+
+
+  constructor(private fetchData: FetchDataService) {}
+
+
+  ngOnInit(): void {
+    this.getCountryCases();
+    this.sortArrays;
+  }
+
+  getCountryCases() {
+    this.fetchData.getAllCountries()
+      .subscribe((countries) => {
+        countries.map((country) => {
+          let cardCases: any = {
+            name: country.country,
+            flag: country.countryInfo.flag,
+            value: country.cases
+          }
+          let cardDeaths: any = {
+            name: country.country,
+            flag: country.countryInfo.flag,
+            value: country.deaths
+          }
+          let cardActives: any = {
+            name: country.country,
+            flag: country.countryInfo.flag,
+            value: country.active
+          }
+          let cardRecovered: any = {
+            name: country.country,
+            flag: country.countryInfo.flag,
+            value: country.recovered
+          }
+          let cardTodayDeaths: any = {
+            name: country.country,
+            flag: country.countryInfo.flag,
+            value: country.todayDeaths
+          }
+          let cardTodayCases: any = {
+            name: country.country,
+            flag: country.countryInfo.flag,
+            value: country.todayCases
+          }
+
+          this.topCases.push(cardCases);
+          this.topDeaths.push(cardDeaths);
+          this.topActives.push(cardActives);
+          this.topRecovered.push(cardRecovered);
+          this.topTodayDeaths.push(cardTodayDeaths);
+          this.topTodayCases.push(cardTodayCases);
+
+        })
+
+      })
+  }
+
+  sortArrays() {
+    this.topCases = this.topCases.sort((a, b) => a.value - b.value);
+    this.topDeaths = this.topDeaths.sort((a, b) => a.value - b.value);
+    this.topActives = this.topActives.sort((a, b) => a.value - b.value);
+    this.topRecovered = this.topRecovered.sort((a, b) => a.value - b.value);
+    this.topTodayDeaths = this.topTodayDeaths.sort((a, b) => a.value - b.value);
+    this.topTodayCases = this.topTodayCases.sort((a, b) => a.value - b.value);
+  }
 
 }
