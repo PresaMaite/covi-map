1464ab9772a927d84205808018f4366bf429c6ee
PresaMaite <m.presa@hotmail.com>
2024-04-23 00:08:02 +0200

    feat(nav): routes fixed

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,37 +1,44 @@
 import { Component } from '@angular/core';
 import { NavButtons } from './navbar.models';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-  navButtons: NavButtons[] = [{
-    icon: "./../../assets/icons/pie-chart.svg",
-    url: " ",
-  }, {
-    icon: "./../../assets/icons/layout-three-columns.svg",
-    url: " ",
-  }, {
-    icon: "./../../assets/icons/columns-gap.svg",
-    url: " ",
-  }, {
-    icon: "./../../assets/icons/folder2.svg",
-    url: " ",
-  }, {
-    icon: "./../../assets/icons/collection.svg",
-    url: " ",
-  }, {
-    icon: "./../../assets/icons/graph-up-arrow.svg",
-    url: " ",
-  }, {
+  navButtons: NavButtons[] = [ {
     icon: "./../../assets/icons/globe.svg",
-    url: " ",
+    url: "/dashboard",
+  },{
+    icon: "./../../assets/icons/collection.svg",
+    url: "/top10",
   }, {
     icon: "./../../assets/icons/chat-left.svg",
-    url: " ",
+    url: "/form",
   }]
 }
+
+
+
+// Por ahora no están activos los siguientes botones 
+
+// {
+//   icon: "./../../assets/icons/pie-chart.svg",
+//   url: " ",
+// }, {
+//   icon: "./../../assets/icons/layout-three-columns.svg",
+//   url: " ",
+// }, {
+//   icon: "./../../assets/icons/columns-gap.svg",
+//   url: " ",
+// }, {
+//   icon: "./../../assets/icons/folder2.svg",
+//   url: " ",
+// }, {
+//   icon: "./../../assets/icons/graph-up-arrow.svg",
+//   url: " ",
+// }
